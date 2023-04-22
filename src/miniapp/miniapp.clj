8fdbf2f8c8f6ef1426710bf82e956503c4e02bc1
Your Name <you@example.com>
2023-04-22 14:41:31 -0700

    wip

--- src/miniapp/miniapp.clj
+++ src/miniapp/miniapp.clj
@@ -4,16 +4,39 @@
             [fbc-utils.core :as ut]
             [clj-pid.core :as pid]
             [fbc-utils.debug]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [wkok.openai-clojure.api :as oa]
+            [markdown.core :as md]
+            [ring.util.codec :as cd]))
 
 (def state (atom {:emacs-modified false
-                  :emacs-text     nil}))
+                  :emacs-text     nil
+                  :chatgpt-query  ""}))
 
-(def code-page (slurp "code_page.js"))
+(def code-page (slurp "code_page.html"))
+
+(def chatgpt-page (slurp "chatgpt_page.html"))
+
+(def openai-api-key (:openai-api-key (read-string (slurp "config.edn"))))
+
+(defn chatgpt [prompt
+               {:keys [system-prompt
+                       stop-seqs]
+                :as   options}]
+  (get-in (oa/create-chat-completion (cond-> {:model    "gpt-3.5-turbo"
+                                              :messages (vec (concat (when system-prompt
+                                                                       [{:role    "system"
+                                                                         :content system-prompt}])
+                                                                     [{:role    "user"
+                                                                       :content prompt}]))}
+                                       stop-seqs (assoc :stop stop-seqs))
+                                     {:api-key openai-api-key})
+          [:choices 0 :message :content]))
 
 (defn request-handler [{:keys [uri
                                body
-                               server-name]
+                               server-name
+                               request-method]
                         :as   req}]
   (case uri
     "/fereofjhh"        (do (swap! state
@@ -28,15 +51,26 @@
                          :body   (let [{:keys [emacs-text]
                                         :as   state} @state]
                                    (st/replace code-page "{}" (or emacs-text "")))}
+    "/mulepantry"       {:status 200
+                         :body   (let [_ #d req
+                                       chatgpt-query (if (= request-method :get)
+                                                       (:chatgpt-query @state)
+                                                       ((cd/form-decode (slurp body)) "query"))]
+                                   (swap! state assoc :chatgpt-query #d chatgpt-query)
+                                   (-> chatgpt-page
+                                       (st/replace "{query}" chatgpt-query)
+                                       (st/replace "{answer}" (if (= chatgpt-query "")
+                                                                ""
+                                                                (md/md-to-html-string (chatgpt chatgpt-query {}))))))}
     "/asdfsdidii"       {:status 200
                          :body   (:emacs-text @state)}
     "/greet"            {:status 200
                          :body   (str "Hello server " (pid/current))}
     "/supertight.woff" (let [font-file   (io/file "supertight.woff")
-                            font-stream (io/input-stream font-file)]
-                        {:status  200
-                         :headers {"Content-Type" "application/font-woff"}
-                         :body    font-stream})
+                             font-stream (io/input-stream font-file)]
+                         {:status  200
+                          :headers {"Content-Type" "application/font-woff"}
+                          :body    font-stream})
     "/favicon.ico"      {:status 200}
     {:status 404}))
 
